6a3c9313f3239bb70dc565d02972de77d1303a74
Liang Sun <i@liangsun.org>
2015-05-30 23:14:38 +0800

    Add mobile config.

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+  id: 'im.youplus.pry',
+  name: 'You+趴一趴',
+  description: '如果趴，请深趴',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon.png',
+  'android_mdpi': 'resources/icons/icon.png',
+  'android_hdpi': 'resources/icons/icon.png',
+  'android_xhdpi': 'resources/icons/icon.png'
+});
